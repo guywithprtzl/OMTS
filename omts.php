d2b9dab316f328a2a8a128f691380e9588a30ff9
MashedPotatoes\Brayden <brayden.dewar@hotmail.com>
2018-03-22 18:01:39 -0400

    Test added files

---
+++ omts.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Brayden
+ * Date: 2018-03-22
+ * Time: 5:22 PM
+ */
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "My first PHP script!";
+?>
+
+</body>
+</html>
