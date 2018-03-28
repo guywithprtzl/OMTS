83cd1efda132b31b8bff138a9417a655ad698518
MashedPotatoes\Brayden <brayden.dewar@hotmail.com>
2018-03-27 22:56:30 -0400

    Added reservation

--- reservations.php
+++ reservations.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>Online Movie Database System</title>
+<body>
+<meta charset="UTF-8">
+
+<table>
+    <tr><th>Movie</th><th>Tickets Reserved</th><th>Theater Name</th><th>Theater Number</th><th>Start Time</th><th></th></tr>
+
+
+
+
 <?php
 /**
  * Created by PhpStorm.
@@ -10,11 +22,26 @@ $dbh = mysqli_connect('127.0.0.1', "root", "", "themovies");
 if (mysqli_connect_errno()){
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-$query = mysqli_query($dbh,"select title, runningTime, rating, synopsis from movie");
+$loginID = $_POST["loginID"];
+$query = mysqli_query($dbh,"select title, ticketsReserved, theaterName, theaterNumber, startTime
+      from  (customer as c right OUTER JOIN reservation as r ON r.accountNumber = c.accountNumber)
+      	where c.loginID = '$loginID'");
 if($query == NULL){
     echo "NULL Query";
 }
+while($row = mysqli_fetch_array($query)) {
+    echo"<tr><td>".$row['title']."</td><td>".$row['ticketsReserved']."</td><td>".$row['theaterName']."</td><td>".$row['theaterNumber']."</td><td>".$row['startTime']."</td>
+    <td>
+    <form action=\"clearReservation.php\" method = \"post\">
+        <input type=\"submit\" name=\"submit\" value=\"Remove Reservation\"><br>
+    </form>
+    </td>
+    
+    </tr>";
+}
 
+?>
 
-
-while($row = mysqli_fetch_array($query)) {;}
+</table>
+</body>
+</html>
